ef7b569a521040dc0cb13ce27ba0fcfba970714f
Tara E. Sabella <tarasabella@gmail.com>
2018-10-07 21:44:25 +0000

    automatically backed up by learn

--- loops.js
+++ loops.js
@@ -1,34 +1,26 @@
 function forLoop(array) {
-	for (let i = 0; i < 25; i++) {
-		if (i === 1) {
-			array.push('I am 1 strange loop.');
-		} else {
-			array.push(`I am ${i} strange loops.`);
-		}
-	}
-
-	return array;
+   for ( let i = 0; i < 25; i++) {
+     if (i === 1) {
+       array.push(`I am 1 strange loop.`)
+     } else {
+       array.push(`I am ${i} strange loops.`)
+     }
+  }
+  return array;
 }
-
-function whileLoop(n) {
-	while (n > 0) {
-		console.log(--n);
-	}
-
-	return 'done';
+ function whileLoop(n) {
+  let countdown = n;
+     while (countdown > 0) {
+      console.log(--countdown)
+    }
+    return "done";
+  }
+ function doWhileLoop(array) {
+  function maybeTrue() {
+    return Math.random() >= 0.5
+  }
+   do {
+    array.pop();
+  } while (array.length > -1 && maybeTrue());
+    return array;
 }
-
-function doWhileLoop(array) {
-	var i = 0;
-
-	function incrementVariable() {
-		i = i + 1;
-	}
-
-	do {
-		console.log('array.length = ' + array.length + ' and i = ' + i);
-		array = array.slice(1);
-		incrementVariable();
-	} while (array.length > 0 && i < 5);
-
-	return array;
